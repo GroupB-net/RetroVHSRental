c9d6124e6fc021186dd8eda30dfc76b5319d78dc
lienglen <li.englen98@gmail.com>
2025-09-09 09:21:33 +0200

    Created MovieController with Index and Details.

---
+++ RetroVHSRental/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RetroVHSRental.Repository;
+
+namespace RetroVHSRental.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly IfilmRepository _filmRepository;
+
+        public MovieController(IfilmRepository _filmRepository)
+        {
+            this._filmRepository = _filmRepository;
+        }
+        public IActionResult Index()
+        {
+            return View(_filmRepository.GetAllFilmsAsync());
+        }
+
+        public IActionResult Details(int id)
+        { 
+            return View(_filmRepository.GetFilmByIdAsync(id));
+        }
+
+    }
+}
